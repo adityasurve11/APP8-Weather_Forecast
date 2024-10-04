9311004661e9b981d642a36bc0f34c5af262b653
ADITYA <aditya11surve08@gmail.com>
2024-10-04 20:36:27 +0530

    made some changes of our own

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ st.title("Weather Forecast For Upcoming Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of days to be forecasted")
+min_value = 1
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+if days == min_value:
+    print(st.subheader(f"{option} for the next {days} day in {place}"))
+else:
+    print(st.subheader(f"{option} for the next {days} days in {place}"))
