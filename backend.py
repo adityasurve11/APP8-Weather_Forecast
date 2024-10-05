c6e7ac2232529d417f71403696da51a26a46abd5
ADITYA <aditya11surve08@gmail.com>
2024-10-05 22:30:06 +0530

    import requests

--- backend.py
+++ backend.py
@@ -1,5 +1,8 @@
+import requests
+
 API_KEY = "ddda12c0b7904a6bde1c490350900fed"
+
 def get_data(place, forecast_days, kind):
-    url = "api.openweathermap.org/data/2.5/forecast?id={cityID}&appid={API_key}"
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     return d, t
 
