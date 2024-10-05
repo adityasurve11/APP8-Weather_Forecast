7dd62395d88a98a2431720a128cd820258204403
ADITYA <aditya11surve08@gmail.com>
2024-10-05 22:20:27 +0530

    make a url function for the api

--- backend.py
+++ backend.py
@@ -1,7 +1,5 @@
 def get_data(place, forecast_days, kind):
-    api.openweathermap.org / data / 2.5 / forecast?id = {city
-    ID} & appid = {API
-    key}
+    url = "api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={API key}"
 
     return d, t
 
