76316abde947f1b989ba307bc12b98d697de9795
ADITYA <aditya11surve08@gmail.com>
2024-10-07 00:44:55 +0530

    added filtered data function

--- backend.py
+++ backend.py
@@ -6,7 +6,7 @@ def get_data(place, forecast_days=None, kind=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-
+    filtered_data = data["list"]
     return data
 
 if __name__=="main":
