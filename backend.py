a49144f2ff1a0d6197566c0fbc41b68b049f533d
ADITYA <aditya11surve08@gmail.com>
2024-10-07 00:47:38 +0530

    added filtered data function4

--- backend.py
+++ backend.py
@@ -9,6 +9,7 @@ def get_data(place, forecast_days=None, kind=None):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
+    if Kind == "Tempearture"
     return data
 
 if __name__=="main":
